710703ec1296e1ceb8610a69645828d106c4697d
Remy Barbe <rbarbe@info-olennax.iut.bx1>
2016-01-27 15:39:20 +0100

    suppression drag

--- www/js/Song.js
+++ www/js/Song.js
@@ -1,5 +1,6 @@
 function Song(){
   this.buffer=null;
+  this.source=null;
   this.statTime=0;
   this.stopTime=0;
 };
@@ -11,21 +12,15 @@ Song.prototype={
     this.source.buffer=this.buffer;
     this.source.connect(audioCtx.destination);
     this.source.start();
-    source=this.source;
   },
   playWithTime:function(time)
   {
     this.source=audioCtx.createBufferSource();
     this.source.buffer=this.buffer;
     this.source.connect(audioCtx.destination);
-    // this.source.loop=true;
     this.source.start(audioCtx.currentTime+time);
   },
   getDuration:function(){
     return this.source.buffer.duration;
-  },
-  getDebut:function(){
-    var left=(this.viewBox.position().left-200)/100;
-    return left;
   }
 }
--- www/js/Timeline.js
+++ www/js/Timeline.js
@@ -1,71 +1,45 @@
 function Timeline(){
-      this.songs=[];
-      this.tempo=80;
-      this.noteTime=(60)/this.tempo/2;
+  this.songs=[];
+  this.tempo=90;
+  this.noteTime=(60)/this.tempo/2;
 }
 
 Timeline.prototype={
-	addSong:function(songElement)
-  	{	
-  		// alert(this.ctx);
-  		// this.ctx=document.getElementById("timeline").getContext("2d");
-  		
-  		// console.log(this.ctx);
-  		//this.ctx=document.getElementById("#timeline").getContext("2d");
-  		//canvas.getContext("2d");
-  		// alert(songElement.localName);
-  		// alert("test");
-  		
-  	},
-  	drawLines:function(){
-  // 		this.ctx.beginPath();
 
+  play:function(){
 
-		// this.ctx.moveTo(0,0);
-		// this.ctx.lineTo(300,150);
-		// this.ctx.stroke();
-  	},
-  	play:function(){
-  		console.log("test");
-  		var self=this;
-  		
-  		
-  		$('#timeline .piste').each(function(){
-  			// console.log(this);
-  			var step=0;
-  			$(this).find('.box').each(function(){
-  			var idSong=$(this).find('.files').attr('data-song-id');
-  			
-  			
-  			if(idSong!=null){
-  				timeline.songs[idSong].playWithTime(step*self.noteTime);
-  			}
-  			step++;
-  		});
+    var self=this;
 
-  		});
+    $('#timeline .piste').each(function(){
+     
+      var step=0;
+      
+      $(this).find('.box').each(function(){
+      
+        var idSong=$(this).find('.files').attr('data-song-id');
+        if(idSong!=null){
+          timeline.songs[idSong].playWithTime(step*self.noteTime);
+        }
+        step++;
 
-  		
-  	},
-  	loadSong:function(id,urlSong){
-  		// console.log(urlSong);
-  		var self=this;
+      });
 
-  		$.ajax({
-		    url: urlSong,
-		    xhrFields : {
-				responseType : 'arraybuffer'
-			},
-		}).done(function(arrayBuffer){
-			audioCtx.decodeAudioData(arrayBuffer, function(buffer) 
-				{
-					var song=new Song();
-					song.buffer=buffer;
-					self.songs[id]=song;
-					// song.play();
+    });
+  },
+  loadSong:function(id,urlSong){
+    var self=this;
 
-				},function(e){"Error with decoding audio data" + e.err});
-		});
-  	}
-
+    $.ajax({
+      url: urlSong,
+      xhrFields : {responseType : 'arraybuffer'},
+    }).
+    done(function(arrayBuffer){
+      audioCtx.decodeAudioData(arrayBuffer, function(buffer) 
+      {
+        var song=new Song();
+        song.buffer=buffer;
+        self.songs[id]=song;
+      },function(e){"Error with decoding audio data" + e.err});
+    });
+  }
 }
