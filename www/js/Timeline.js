1c16f697e2eb3de0ff89bbbb08deaecf9b9a6b67
Emmanuel Patrois <emmanuel.patrois@etu.u-bordeaux.fr>
2016-01-27 11:38:26 +0100

    play music

--- www/js/Timeline.js
+++ www/js/Timeline.js
@@ -1,6 +1,7 @@
 function Timeline(){
       this.songs=[];
-      this.ctx=this.ctx=document.getElementById("timeline").getContext("2d");
+      this.tempo=80;
+      this.noteTime=(60)/this.tempo/2;
 }
 
 Timeline.prototype={
@@ -17,11 +18,54 @@ Timeline.prototype={
   		
   	},
   	drawLines:function(){
-  		this.ctx.beginPath();
+  // 		this.ctx.beginPath();
 
 
-		this.ctx.moveTo(0,0);
-		this.ctx.lineTo(300,150);
-		this.ctx.stroke();
+		// this.ctx.moveTo(0,0);
+		// this.ctx.lineTo(300,150);
+		// this.ctx.stroke();
+  	},
+  	play:function(){
+  		console.log("test");
+  		var self=this;
+  		
+  		
+  		$('#timeline .piste').each(function(){
+  			console.log(this);
+  			var step=0;
+  			$(this).find('.box').each(function(){
+  			var idSong=$(this).attr('data-song-ref');
+  			
+  			
+  			if(idSong!=null){
+  				timeline.songs[idSong].playWithTime(step*self.noteTime);
+  			}
+  			step++;
+  		});
+
+  		});
+
+  		
+  	},
+  	loadSong:function(id,urlSong){
+  		console.log(urlSong);
+  		var self=this;
+
+  		$.ajax({
+		    url: urlSong,
+		    xhrFields : {
+				responseType : 'arraybuffer'
+			},
+		}).done(function(arrayBuffer){
+			audioCtx.decodeAudioData(arrayBuffer, function(buffer) 
+				{
+					var song=new Song();
+					song.buffer=buffer;
+					self.songs[id]=song;
+					// song.play();
+
+				},function(e){"Error with decoding audio data" + e.err});
+		});
   	}
+
 }
